6593aff0b8da06adf136fd5dd4a8d33bb1dfa906
djerbiano <saberghoudi2222@hotmail.fr>
2026-09-10 16:07:40 +0200

    CurvePoint - mapping entité JPA + constructeur requis par les tests

--- src/main/java/com/nnk/springboot/domain/CurvePoint.java
+++ src/main/java/com/nnk/springboot/domain/CurvePoint.java
@@ -1,15 +1,42 @@
 package com.nnk.springboot.domain;
 
-import org.hibernate.validator.constraints.Length;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+
 import java.sql.Timestamp;
 
 
 @Entity
 @Table(name = "curvepoint")
+@Getter
+@Setter
+@NoArgsConstructor
 public class CurvePoint {
-    // TODO: Map columns in data table CURVEPOINT with corresponding java fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @NotNull(message = "CurveId is mandatory")
+    private Integer curveId;
+
+    private Timestamp asOfDate;
+
+    @NotNull(message = "Term is mandatory")
+    private Double term;
+
+    @NotNull(message = "Value is mandatory")
+    private Double value;
+
+    private Timestamp creationDate;
+
+    public CurvePoint(Integer curveId, Double term, Double value) {
+        this.curveId = curveId;
+        this.term = term;
+        this.value = value;
+    }
 }
