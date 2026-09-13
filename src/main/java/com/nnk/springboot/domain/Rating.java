8aa8915d5c86dfde816d05014b47a90537de9495
djerbiano <saberghoudi2222@hotmail.fr>
2026-09-13 15:31:15 +0200

    Ajout d'un identifiant minimal sur Rating, RuleName, Trade et BidList pour permettre le démarrage de l'application

--- src/main/java/com/nnk/springboot/domain/Rating.java
+++ src/main/java/com/nnk/springboot/domain/Rating.java
@@ -9,4 +9,7 @@ import java.sql.Timestamp;
 @Table(name = "rating")
 public class Rating {
     // TODO: Map columns in data table RATING with corresponding java fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
 }
--- src/main/java/com/nnk/springboot/domain/RuleName.java
+++ src/main/java/com/nnk/springboot/domain/RuleName.java
@@ -8,4 +8,7 @@ import java.sql.Timestamp;
 @Table(name = "rulename")
 public class RuleName {
     // TODO: Map columns in data table RULENAME with corresponding java fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
 }
