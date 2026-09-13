5af102c9f56f89c2aef36eefa36ba30f0bd56094
djerbiano <saberghoudi2222@hotmail.fr>
2026-09-13 17:11:47 +0200

    Correction message d'erreur d'authentification trop explicite

--- src/main/java/com/nnk/springboot/security/CustomUserDetailsService.java
+++ src/main/java/com/nnk/springboot/security/CustomUserDetailsService.java
@@ -18,7 +18,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
         if (user == null) {
-            throw new UsernameNotFoundException("User nonnnnnnn found"+username);
+            throw new UsernameNotFoundException("Invalid username or password "+username);
         }
         return new org.springframework.security.core.userdetails.User(
                 user.getUsername(),
