37dcbed1ce84150b0c6e7b6d072e8f2e41af5a3c
djerbiano <saberghoudi2222@hotmail.fr>
2026-09-17 21:30:31 +0200

    RuleName: entite, service, controller CRUD, correction des templates (sqlStr, liens Cancel, menu navigation)

--- src/main/java/com/nnk/springboot/domain/RuleName.java
+++ src/main/java/com/nnk/springboot/domain/RuleName.java
@@ -2,13 +2,37 @@ package com.nnk.springboot.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import java.sql.Timestamp;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 
 @Entity
 @Table(name = "rulename")
+@Getter
+@Setter
+@NoArgsConstructor
 public class RuleName {
     // TODO: Map columns in data table RULENAME with corresponding java fields
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    private String description;
+    private String json;
+    private String template;
+    private String sqlStr;
+    private String sqlPart;
+
+    public RuleName(String name, String description, String json, String template, String sqlStr, String sqlPart) {
+        this.name = name;
+        this.description = description;
+        this.json = json;
+        this.template = template;
+        this.sqlStr = sqlStr;
+        this.sqlPart = sqlPart;
+    }
 }
---
+++ src/main/java/com/nnk/springboot/services/RuleNameService.java
@@ -0,0 +1,34 @@
+package com.nnk.springboot.services;
+
+import com.nnk.springboot.domain.RuleName;
+import com.nnk.springboot.exceptions.ResourceNotFoundException;
+import com.nnk.springboot.repositories.RuleNameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RuleNameService {
+
+    @Autowired
+    private RuleNameRepository ruleNameRepository;
+
+    public List<RuleName> findAll() {
+        return ruleNameRepository.findAll();
+    }
+
+    public RuleName findById(Integer id) {
+        return ruleNameRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Invalid ruleName Id:" + id));
+    }
+
+    public RuleName save(RuleName ruleName) {
+        return ruleNameRepository.save(ruleName);
+    }
+
+    public void deleteById(Integer id) {
+        RuleName ruleName = findById(id);
+        ruleNameRepository.delete(ruleName);
+    }
+}
