d8aac52e92c135b214deb510e2443d0d58c68d63
djerbiano <saberghoudi2222@hotmail.fr>
2026-09-19 11:50:30 +0200

    Nettoyage de LoginController: suppression de la methode inutilisee getAllUserArticles

--- src/main/java/com/nnk/springboot/controllers/LoginController.java
+++ src/main/java/com/nnk/springboot/controllers/LoginController.java
@@ -11,8 +11,6 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("app")
 public class LoginController {
 
-    @Autowired
-    private UserRepository userRepository;
 
     @GetMapping("login")
     public ModelAndView login() {
@@ -21,18 +19,10 @@ public class LoginController {
         return mav;
     }
 
-    @GetMapping("secure/article-details")
-    public ModelAndView getAllUserArticles() {
-        ModelAndView mav = new ModelAndView();
-        mav.addObject("users", userRepository.findAll());
-        mav.setViewName("user/list");
-        return mav;
-    }
-
     @GetMapping("error")
     public ModelAndView error() {
         ModelAndView mav = new ModelAndView();
-        String errorMessage= "You are not authorized for the requested data.";
+        String errorMessage = "You are not authorized for the requested data.";
         mav.addObject("errorMsg", errorMessage);
         mav.setViewName("403");
         return mav;
