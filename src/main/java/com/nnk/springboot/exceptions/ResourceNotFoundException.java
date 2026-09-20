4375bf615d13269397c27864b014a540bb834222
djerbiano <saberghoudi2222@hotmail.fr>
2026-09-20 22:20:48 +0200

    JavaDoc de ResourceNotFoundException

--- src/main/java/com/nnk/springboot/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/nnk/springboot/exceptions/ResourceNotFoundException.java
@@ -1,6 +1,18 @@
 package com.nnk.springboot.exceptions;
 
+/**
+ * Exception levée lorsqu'une ressource (entité) recherchée par son
+ * identifiant n'existe pas en base de données. Interceptée par
+ * {@link GlobalExceptionHandler} pour afficher un message d'erreur
+ * clair à l'utilisateur, plutôt qu'une erreur technique brute.
+ */
 public class ResourceNotFoundException extends RuntimeException {
+
+    /**
+     * Construit l'exception avec un message décrivant la ressource introuvable.
+     *
+     * @param message le message d'erreur, typiquement l'identifiant recherché
+     */
     public ResourceNotFoundException(String message) {
         super(message);
     }
